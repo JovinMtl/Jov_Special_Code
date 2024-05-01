906a083f0dccf52134f6967db8ee3f9899a4bbd9
JovinMtl <nsanzumukizath@gmail.com>
2024-05-01 13:16:31 +0200

    stating the reason why of this stringTolist challenge

---
+++ stringTolist.py
@@ -0,0 +1,12 @@
+"""I have intend of converting a string of list into a native list.
+SCENARIO:
+
+jove = " [{'date': '2025-04', 'qte': 4, 'code_operation': '12dxx9'}, {'date': '2024-08', 'qte': 7, 'code_operation': '23dd'}] "
+
+#to be into
+
+Jove = [
+    {'date': '2025-04', 'qte': 4, 'code_operation': '12dxx9'},
+    {'date': '2024-08', 'qte': 7, 'code_operation': '23dd'}
+]
+"""
