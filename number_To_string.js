07a8fd584b8f1368d8f5a69c82e8b4c23507b7d4
JovinMtl <nsanzumukizath@gmail.com>
2024-05-16 13:47:48 +0200

    initial js code that works is implemented

--- number_To_string.js
+++ number_To_string.js
@@ -1,4 +1,22 @@
 
 
-
+const number_To_string = (value=10000) => {
+    // This function is designed to format a number as a string
+    // in a format of separation by 3digits
+    let to_convert = value.toString()
+    let data_length = to_convert.length
+    let i = 0
+    let converted = ''
+    let reversed = ''
+    reversed = to_convert.split('').reverse().toString().replaceAll(',','')
+    for(i=0; i < data_length; i++){
+        if(i%3==0){
+            converted =  converted.concat(`.${reversed[i]}`)
+        } else {
+            converted = converted.concat(`${reversed[i]}`)
+        }
+    }
+    converted = converted.replace('.', '').split('').reverse().toString().replaceAll(',','')
+    return converted
+}
 
